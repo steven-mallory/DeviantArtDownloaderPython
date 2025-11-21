3f25fc15c2a939a280384a3fbcaad2e5a8bba1ca
steven-mallory <steven-mallory@wirelessprv-10-192-214-52.near.illinois.edu>
2025-11-20 23:39:30 -0600

    flask or something

--- DeviantArtDownloaderPython/src/core/runWithAPI.py
+++ DeviantArtDownloaderPython/src/core/runWithAPI.py
@@ -1,6 +1,6 @@
 import secrets
-import urllib.parse
-import Flask #this allows us to receive the GET request to our local server
+import urllib.parse #urllib.parse.urlencode
+from flask import Flask, request #this allows us to receive the GET request to our local server
 CLIENT_id = input("Client id\n")
 
 
@@ -17,8 +17,13 @@ params = {
 fullUrl = oauthString + urllib.parse.urlencode(params)
 
 
-
-print(fullUrl)
+app = Flask(__name__)
+@app.route('/')
+def index():
+    print('args:', request.args)
+    return request.args.get('data', 'none')
+app.run(port=80, debug=True)
+#print(fullUrl)
 #get authorization code
 
 
